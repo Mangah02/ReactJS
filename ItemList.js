9e97a459bb09f564ff9d618f6520f8ffb6352d7e
Joel Manga <jmanga86@gmail.com>
2023-03-06 16:44:20 +0300

    itemlist

---
+++ ItemList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ItemList(props) {
+  return (
+    <ul>
+      {props.items.map(item => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default ItemList;
